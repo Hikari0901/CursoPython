1d2d87b6149534dd38aeee9a5d1433b179c60bd5
unknown <rexj321@gmail.com>
2024-05-28 22:13:04 -0500

    Aprendiendo Cadenas en Python

---
+++ 02-cadenas/practica2.py
@@ -0,0 +1,11 @@
+"""
+Crea un código que imprima en pantalla la siguiente 
+expresión (pero usando print una sola vez)
+Solución:
+A  B  C
+D  E  F
+G  H  I 
+"""
+"""print('"A" + " " + "B" + " " + "C" \n "D" + " " + "E" + " " + "F" \n "G" + " " + "H" + " " + "I"')"""
+
+print(" A  B  C \n D  E  F \n G  H  I")
