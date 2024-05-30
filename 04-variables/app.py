451a98720e83cd680ab0d7318e5c12146f59637a
unknown <rexj321@gmail.com>
2024-05-29 20:01:09 -0500

    Clase de Variables

---
+++ 04-variables/app.py
@@ -0,0 +1,18 @@
+""" mutable : se puede editar la info 
+cuando tiene comillas es extreme
+
+"""
+nombre = "Juan"
+print(nombre)
+nombre = "Laura"
+nombre = "Jose"
+print(nombre)
+
+#*declarar y definir bien la variable claro y conciso si es colores es colores si es fruta es fruta,etc 
+xx = "Tomate"
+print(xx)
+
+edad1 = 30
+edad2 = 60
+edadTotal = edad1 + edad2
+print("Edad Total: " + " " + {edadTotal} )
