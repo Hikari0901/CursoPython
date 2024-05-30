6874483a9cc9f9374dff0e2294a082e90c50a526
unknown <rexj321@gmail.com>
2024-05-29 21:58:07 -0500

    Formato cadenas

---
+++ 07-FormatoCadenas/app.py
@@ -0,0 +1,12 @@
+x = 10
+y = 5
+z = x + y
+#Primera forma de convertir cadenas antes
+print("La suma de {} y {} es igual a = {} ".format(x,y,z))
+
+#actualizacion de nuevas versiones de python
+color = "rojo"
+matricula = 541923
+
+print(f"El auto es {color} y su matricula es {matricula}")
+#f : funcion matematica muestra la informacion de las variables
---
+++ 07-FormatoCadenas/practica2.py
@@ -0,0 +1,12 @@
+"""
+PRACTICA 2
+Necesitamos imprimir los puntos_nuevos y sumar 20 para
+obtener un total de puntos.  
+dentro de la siguiente frase: 
+Has ganado (puntos_nuevos) puntos! En total, acumulas (puntos_totales) puntos
+utilizar el segundo metodo
+"""
+puntos_nuevos = 6
+print(input("Cuanto es 2 + 2 = "))
+print("Felicidades!!!\n")
+print(f"Has ganado {puntos_nuevos} puntos! En total, acumulas {puntos_nuevos + 20} puntos")
